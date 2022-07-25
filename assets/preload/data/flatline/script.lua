6c98931014addb6659071e79af0510af7502f5ac
Microondas Explosivo <100734993+Microondas261477@users.noreply.github.com>
2022-07-25 14:07:19 -0500

    custom notes and difficulty update

---
+++ assets/preload/data/flatline/script.lua
@@ -0,0 +1,12 @@
+function onCreate()
+    setPropertyFromClass('GameOverSubstate', 'deathSoundName', 'rip bozo'); --put in mods/sounds/
+	setPropertyFromClass('GameOverSubstate', 'loopSoundName', 'Nightshow'); --put in mods/music/
+	setPropertyFromClass('GameOverSubstate', 'endSoundName', 'Retryshow'); --put in mods/music/
+end
+
+function onGameOver()
+	-- You died! Called every single frame your health is lower (or equal to) zero
+	--return Function_Stop if you want to stop the player from going into the game over screen
+    playSound('operate', 1)
+    return Function_Continue;
+end
---
+++ assets/preload/data/hellcat/script.lua
@@ -0,0 +1,21 @@
+function onCreate()
+    setPropertyFromClass('GameOverSubstate', 'deathSoundName', 'rip bozo'); --put in mods/sounds/
+	setPropertyFromClass('GameOverSubstate', 'loopSoundName', 'Nightshow'); --put in mods/music/
+	setPropertyFromClass('GameOverSubstate', 'endSoundName', 'Retryshow'); --put in mods/music/
+end
+
+function onEvent(name)
+    if name == 'industrial society and its future' then
+        ofs = 0
+    end
+    if name == 'original name' then
+        ofs = 40
+    end
+end
+
+function onGameOver()
+	-- You died! Called every single frame your health is lower (or equal to) zero
+	-- return Function_Stop if you want to stop the player from going into the game over screen
+    playSound('ms fortune win', 1)
+    return Function_Continue;
+end
