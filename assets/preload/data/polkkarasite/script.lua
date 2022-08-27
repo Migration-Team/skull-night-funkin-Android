2043bb1fc0b6cf23106cd85b6bc7e2383eb25988
Microondas Explosivo <100734993+Microondas261477@users.noreply.github.com>
2022-08-27 18:28:47 -0500

    final files update

---
+++ assets/preload/data/polkkarasite/script.lua
@@ -0,0 +1,37 @@
+function onCreate()
+    setPropertyFromClass('GameOverSubstate', 'deathSoundName', 'rip bozo'); --put in mods/sounds/
+	setPropertyFromClass('GameOverSubstate', 'loopSoundName', 'Nightshow'); --put in mods/music/
+	setPropertyFromClass('GameOverSubstate', 'endSoundName', 'Retryshow'); --put in mods/music/
+
+    SamsonTalks = true
+end
+
+
+
+function opponentNoteHit(id, direction, noteType, isSustainNote)
+    if noteType == 'Hair_Note' and SamsonTalks == true then
+        if getProperty('dad.curCharacter') == 'filia' then
+          triggerEvent('Change Character', 'dad', 'samson')
+          runTimer('back', 1)
+        end
+    end
+end
+
+function onTimerCompleted(tag)
+    if tag == 'back' then
+        triggerEvent('Change Character', 'dad', 'filia')
+    end
+end
+
+
+function onGameOver()
+	-- You died! Called every single frame your health is lower (or equal to) zero
+	--return Function_Stop if you want to stop the player from going into the game over screen
+    playSound('filia retry', 1)
+end
+
+function onBeatHit()
+    if curBeat == 447 then
+        SamsonTalks = false
+    end
+end
