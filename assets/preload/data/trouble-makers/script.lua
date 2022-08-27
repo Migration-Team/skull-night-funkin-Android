41dfaa3ec99010590816dbf6ea1955e69a236ec5
Microondas Explosivo <100734993+Microondas261477@users.noreply.github.com>
2022-08-27 18:25:56 -0500

    final files update

--- assets/preload/data/trouble-makers/script.lua
+++
@@ -1,5 +0,0 @@
-function onCreatePost()
-    setPropertyFromClass('GameOverSubstate', 'deathSoundName', 'rip bozo'); --put in mods/sounds/
-	setPropertyFromClass('GameOverSubstate', 'loopSoundName', 'Nightshow'); --put in mods/music/
-	setPropertyFromClass('GameOverSubstate', 'endSoundName', 'Retryshow'); --put in mods/music/
-end
