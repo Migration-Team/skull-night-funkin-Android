4aee35c3446ab127162dff2867ac90ae2e2f699b
Microondas Explosivo <100734993+Microondas261477@users.noreply.github.com>
2022-09-03 21:06:14 -0500

    post beta update

--- assets/preload/data/hairmess/script.lua
+++ assets/preload/data/hairmess/script.lua
@@ -1,7 +1,55 @@
-function onCreatePost()
+local revengeOfTheBlackScreen = false
+
+function onCreate()
+
+	precacheImage('characters/Samson')
+
     setPropertyFromClass('GameOverSubstate', 'deathSoundName', 'rip bozo'); --put in mods/sounds/
 	setPropertyFromClass('GameOverSubstate', 'loopSoundName', 'Nightshow'); --put in mods/music/
 	setPropertyFromClass('GameOverSubstate', 'endSoundName', 'Retryshow'); --put in mods/music/
+
+	makeLuaSprite ('black', uhh, -300, -200)
+	setScrollFactor('black', 0, 0)
+	luaSpriteMakeGraphic('black', 2000, 1500, '000000')
+	setProperty('black.antialiasing',false)
+
+	makeLuaSprite ('light', 'effects/spotlight', -100,0)
+	setScrollFactor('light', 0, 0)
+	scaleObject('light', 1.1, 1)
+	setProperty('light.antialiasing',false)
+
+	addLuaSprite('light', true)
+	addLuaSprite('black', true)
+
+end
+
+function onStepHit()
+	if curStep == 47 then
+		setProperty('black.alpha', 0)
+	end
+	if curStep == 304 then
+		removeLuaSprite('light')
+		if flashingLights then
+		   cameraFlash('hud', 'ffffff', 0.7, false)
+		end
+	end
+	if curStep == 992 then
+        revengeOfTheBlackScreen = true
+	end
+	if curStep == 1024 then
+		revengeOfTheBlackScreen = false
+		removeLuaSprite('black')
+		if flashingLights then
+			cameraFlash('hud', 'ffffff', 0.7, false)
+		end
+	end
+end
+
+
+function onUpdate()
+	if revengeOfTheBlackScreen == true then
+		setProperty('black.alpha', getProperty('black.alpha') + 0.02)
+	end
 end
 
 function onGameOver()
